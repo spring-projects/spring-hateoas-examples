1bda98f9f5c91c39ee83b98686ef43e47c550479
Greg Turnquist <gturnquist@pivotal.io>
2019-03-03 12:38:04 -0600

    Fix build.sh script.

--- ci/build.sh
+++ ci/build.sh
@@ -4,11 +4,11 @@ set -euo pipefail
 
 [[ -d $PWD/maven && ! -d $HOME/.m2 ]] && ln -s $PWD/maven $HOME/.m2
 
-spring_hateoas-examples_artifactory=$(pwd)/spring-hateoas-examples-artifactory
+spring_hateoas_examples_artifactory=$(pwd)/spring-hateoas-examples-artifactory
 
-rm -rf $HOME/.m2/repository/org/springframework/hateoas-examples 2> /dev/null || :
+rm -rf $HOME/.m2/repository/org/springframework/hateoas 2> /dev/null || :
 
 cd spring-hateoas-examples-github
 
 ./mvnw -Pdistribute -Dmaven.test.skip=true clean deploy \
-	-DaltDeploymentRepository=distribution::default::file://${spring_hateoas-examples_artifactory}
+	-DaltDeploymentRepository=distribution::default::file://${spring_hateoas_examples_artifactory}
