c80d1ff6f13d3cc64d40cabfee10d06caba2c874
Greg Turnquist <gturnquist@pivotal.io>
2019-06-08 07:43:47 -0500

    Introduce Jenkins.

--- ci/build.sh
+++ ci/build.sh
@@ -2,13 +2,4 @@
 
 set -euo pipefail
 
-[[ -d $PWD/maven && ! -d $HOME/.m2 ]] && ln -s $PWD/maven $HOME/.m2
-
-spring_hateoas_examples_artifactory=$(pwd)/spring-hateoas-examples-artifactory
-
-rm -rf $HOME/.m2/repository/org/springframework/hateoas 2> /dev/null || :
-
-cd spring-hateoas-examples-github
-
-./mvnw -Pdistribute -Dmaven.test.skip=true clean deploy \
-	-DaltDeploymentRepository=distribution::default::file://${spring_hateoas_examples_artifactory}
+./mvnw -P${PROFILE} -Dmaven.test.skip=true clean deploy -B
